55431042d7928d0f373d45104066a713db91bb41
lalitnailwal <lalitnailwal@hotmail.com>
2023-03-08 19:30:29 +0530

    get the param value using ActivatedRoute interface & get the param value from parent and child route

--- bookStoreApp/src/app/public/public.component.ts
+++ bookStoreApp/src/app/public/public.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-public',
   templateUrl: './public.component.html',
   styleUrls: ['./public.component.scss']
 })
-export class PublicComponent {
+export class PublicComponent implements OnInit {
+  bookId: Number = 1;
+  authorId: Number = 90;
 
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.firstChild?.params.subscribe((param)=>{
+      console.log(param);
+    })
+  }
 }
