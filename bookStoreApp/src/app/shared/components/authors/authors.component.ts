4c2055eb73eaec57c7230e312915d9a5e01a00aa
lalitnailwal <lalitnailwal@hotmail.com>
2023-03-16 09:43:54 +0530

    Pass data from parent to child component

--- bookStoreApp/src/app/shared/components/authors/authors.component.ts
+++ bookStoreApp/src/app/shared/components/authors/authors.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-authors',
   templateUrl: './authors.component.html',
   styleUrls: ['./authors.component.scss']
 })
-export class AuthorsComponent {
 
+export class AuthorsComponent {
+  @Input() data: number;
+  @Input() showAuthors: boolean;
 }
